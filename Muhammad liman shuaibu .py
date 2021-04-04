0df00eb8c0fd4e3ec75399caf9fb14f13b176226
Pyper08 <81815792+Pyper08@users.noreply.github.com>
2021-04-04 20:02:51 +0100

    Add files via upload

---
+++ Muhammad liman shuaibu .py
@@ -0,0 +1,60 @@
+"""
+Write a short Python code to display the name of your name & department
+"""
+name = "Abubakar  Imran Sadiq"
+department = "Computer Science"
+print(name)
+print(department)
+
+print("===`°¶^||°===")
+
+"""
+Assign the string "Boo!" to the variable scare and print the variable.
+"""
+scare = "Boo!"
+print(scare)
+
+
+
+"""
+Add two variables and assign the result to a third variable. print all the variable names.
+"""
+num1 = 10
+num2 = 5
+result =	num1 + num2
+print(num1)
+print(num2)
+print(result)
+
+print("===^^^^____^^^^===")
+"""
+Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
+"""
+outcome =	12/.5
+print(outcome)
+
+print
+print("===^^^^∆∆^^^^===")
+"""
+Assign the sum of two numbers to a variable and print the variable.
+"""
+sumValues = 3 + 5
+print(sumValues)
+
+
+"""
+Write the code that finds the remainder when 8 is divided by 3
+"""
+remainder =	8/3
+print(remainder)
+
+print("===^^^^°•°•°•°•^^^^===")
+"""
+Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result.
+x = 2 * 4 * 4 + 2
+"""
+multiple = 2 * 4
+added =	4 + 2
+
+result =	multiple * added
+print(result)
