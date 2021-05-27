53c3eb98e5393b6ff50cee286ba49270ecca9630
Pyper08 <81815792+Pyper08@users.noreply.github.com>
2021-05-28 00:35:02 +0100

    Add files via upload

---
+++ Liman.py
@@ -0,0 +1,33 @@
+#Muhammad liman shuaibu
+#U19/fns/csc/1038
+# USSD code for Zenith Bank(Open account)
+#(*966#)
+code=input("")
+if(code=='*966#'):
+	print("Welcome to Eazybanking by Zenith Bank")
+	print("1>Open Account\n2>Get a Card\n3>Register\n4>CheckBalance\n5>Airtime\n6>Transfers\n7>Self Service\n8>Next")
+option=input("")
+if(option=='1'):
+	print('eaZybanking\nPlease select an option below:')
+	print("1>Open Acct For Self\n2>Open Acct For 3rd Party\n3>Create Mobile Wallet\n4>Upgrade ZWallet to Acct\n5>Next")
+option2=input("")
+if (option2=='1'):
+	print('eaZybanking\nWould you like to open the account with your BVN number?\nDial *565*0# to get your bvn\n1>Yes (Eazy save acct)\n2>No (Eazy acct)')
+option3=input("")
+if (option3=='1'):
+	print("eaZybanking\nAccount Opening\nEnter your BVN:")
+Bvn=input("")
+
+print("eaZybanking\nAccount Opening\nPlease enter your BVN Date of Birth in the format (dd/mm/yyyy)")
+
+date=input("")
+print("eaZybanking\nAccount Opening\nEnter your state of residence:")
+
+state=input("")
+print('eaZybanking.')
+
+	
+
+
+
+	
